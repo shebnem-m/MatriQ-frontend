48ed9f158b6cb1ece59c50ea33bbb3d0ff7b8cc6
TheGlitch26 <nihad.bagirzade888@gmail.com>
2026-07-15 02:37:01 +0400

    fix: add configuration for images

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         port: "8080",
         pathname: "/uploads/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async rewrites() {
